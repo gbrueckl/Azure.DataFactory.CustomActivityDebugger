b6d23c50f4b6a858590b6d2acc0e6bb1fcd6bf9e
gbrueckl <gbrueckl@HP840-GBRUECKL>
2016-11-08 21:47:00 +0100

    chagnes for documentation

--- ADFCustomActivityDebugger/Program.cs
+++ ADFCustomActivityDebugger/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Management.DataFactories.Runtime;
 
 // Namespace of ADF Custom Activity
 using DataDownloaderActivityNS;
@@ -10,27 +11,38 @@ using DataDownloaderActivityNS;
 namespace Azure.DataFactory
 {
     /*
-    This Class should help you to debug your ADF Custom Activities. Here are the steps to make this work:
-    1) Create a new C# Console Project within the same solution as your ADF Custom Activities C# Project
-    2) Add a Reference to the ADF Custom Activity C# Project 
-    3) Add "using <your namespace>;" (=the namespace of your ADF Custuom Activity)
-    4) Change the fixed values in the Main() function below to match your environment
+    This C# Console Project should help you to debug your ADF Custom Activities. Here are the steps to make it work:
+    1) Add a Reference to the ADF Custom Activity C# Project (e.g. MyADFProject) - must reside in same Solution!
+    3) Add the Namespace of your ADF Custom Activity to this file: "using <your namespace>;" (e.g. using DataDownloaderActivityNS)
+    4) Change the static values in the Program-Class below to match your environment
+    5) Set Breakpoints in your Custom Activity Code
+    6) Start the Debugger as any other regular console application
     */
     public class Program
     {
+        // change all these variables according to your ADF Custom Activity
+        private static string ADFWorkingDirectory = @"d:\Work\SourceControl\GitHub\Azure.DataFactory.CustomActivityDebugger\MyADFProject\"; // directory of the ADF solution
+        private static string ADFPipelineName = "DataDownloaderSamplePipeline"; // name of the pipeline which contains the Activity to test
+        private static string ADFActivityName = "DownloadData"; // name of the Activity to test
+        private static string ADFConfigurationFile = null; // e.g. "MyConfig.json", optional (must reside in the ADFWorkingDirectory!)
+        private static DateTime SliceStart = new DateTime(2016, 10, 07); // set Slice-StartDate
+        private static DateTime SliceEnd = new DateTime(2016, 10, 07); // set Slice-EndDate
+        private static IDotNetActivity executeable = new DataDownloaderActivity(); // the IDotNetActivity that should be debugged
+
+
         static void Main(string[] args)
         {
             ADFCustomActivityDebugger debugger = new ADFCustomActivityDebugger(
-                                                        @"d:\Work\SourceControl\GitHub\Azure.DataFactory.CustomActivityDebugger\MyADFProject\", // directory of the ADF solution
-                                                        "DataDownloaderSamplePipeline", // name of the pipeline which contains the Activity to test
-                                                        "DownloadData", // name of the Activity to test
-                                                        null); // optional name of a config-file (must reside in the same folder as all other files!)
-            debugger.SliceStart = new DateTime(2016, 10, 07); // set SliceStart if required
-            debugger.SliceEnd = new DateTime(2016, 10, 08); // set SliceEnd if required
+                                                        ADFWorkingDirectory,
+                                                        ADFPipelineName, 
+                                                        ADFActivityName,
+                                                        ADFConfigurationFile);
+            debugger.SliceStart = SliceStart;   
+            debugger.SliceEnd = SliceEnd; 
 
             debugger.LoadADFEnvironment(); // Load the Environment
 
-            debugger.ExecuteActivity(new DataDownloaderActivity()); // Execute your Activity
+            debugger.ExecuteActivity(executeable); // Execute your Activity
         }
     }
 }
